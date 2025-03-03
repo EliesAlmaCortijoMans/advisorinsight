93f6ee3249adef527cadcf7f71a4d6badde0eb63
maliktalha3702 <zubair.m@turing.com>
2025-03-03 17:07:24 +0500

    Removed ignored files from repository

--- server/constants/constants.py
+++
@@ -1,3 +0,0 @@
-COMPANIES_LIST = ["AAPL", "TSLA", "NVDA", "WMT", "IBM", "GME", "MSFT"]
-FINNHUB_BASE_URL = "https://finnhub.io/api/v1"
-FINNHUB_TRANSCRIPT_URL = "https://finnhub.io/api/v1/stock/transcripts/list"
